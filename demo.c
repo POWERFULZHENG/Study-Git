3a2b07a55ae0d223011761c06526d2d65999c98c
xgz_glb <goodMorning_glb@atguigu.com>
2023-01-07 10:29:17 +0800

    在main上优化了sum()函数

--- demo.c
+++ demo.c
@@ -4,12 +4,12 @@
 int sum(int a,int b)
 {
 	int c = 0;
-	c = a + b ;
-	return c;
+	return c = a+b;//master所做的修改
 }
 int main()
 {
 	int a = 0;
+	a = sum;//version1所做的修改
 	printf("%d\n",a);
 	return 0;
 }
